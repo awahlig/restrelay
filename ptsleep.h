8c40147dcbc97ed2576f5988a5cbc570eca0558b
Arkadiusz Wahlig <arkadiusz@wahlig.eu>
2022-04-16 22:56:44 +0200

    improve PT_SLEEP

--- ptsleep.h
+++ ptsleep.h
@@ -3,8 +3,9 @@
 
 #include <pt.h>
 
-#define PT_SLEEP(pts, millis_var, t) { \
+#define PT_SLEEP(pts, millis_var, t) do { \
     (millis_var) = millis(); \
-    PT_WAIT_UNTIL((pts), millis() - (millis_var) > (t)); }
+    PT_WAIT_UNTIL((pts), millis() - (millis_var) > (t)); \
+    } while (0)
 
 #endif /* PTSLEEP_H */
